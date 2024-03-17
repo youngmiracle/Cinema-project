35419ed474079b9d6a393d2c0451d1cd5fc8a2ec
Edward <leventase@mail.ru>
2024-03-17 21:50:53 +0900

    Add NavTabs, Header and other jsx page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,27 @@
 import './App.css';
+import {Header} from './components/widgets/Header';
+import {MainPage} from './components/pages/Main/Main';
+import {ContactsPage} from './components/pages/Contacts/Contacts';
+import {InformationPage} from './components/pages/Information/Information';
+import {PricesPage} from './components/pages/prices/Prices';
+import { useState } from 'react';
+
+
 
 export function App() {
+  const [nav, setNav] = useState('main')
+
+
   return (
     <>
-      
+      <Header active={nav} onChange={(current) => setNav(current)}>
+      </Header>
+      <main>
+        {nav === 'main' && <MainPage/>}
+        {nav === 'contacts' && <ContactsPage/>}
+        {nav === 'info' && <InformationPage/>}
+        {nav === 'prices' && <PricesPage/>}
+      </main>
     </>
   );
 }
---
+++ src/components/pages/prices/Prices.jsx
@@ -0,0 +1,15 @@
+import './prices.css'
+
+
+export function PricesPage (){
+    
+    return(
+        <section className="pricesSection">
+            <h1 style={{
+                textAlign: 'center',
+                color: 'white',
+                fontSize: '28px'
+            }}>Тут будут цены</h1>
+        </section>
+    )
+}

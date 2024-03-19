18d0c05b6d590cc009425b7929b232587b42067d
Edward <leventase@mail.ru>
2024-03-20 01:19:08 +0900

    Add style for Header and Page Contacts 19.03.2024

--- src/components/pages/Contacts/Contacts.jsx
+++ src/components/pages/Contacts/Contacts.jsx
@@ -26,7 +26,6 @@ const asideInformation = [
         title: "Вопросы трудоустройства",
         number: '+7 914 666-66-66',
         hrefMail: "mailto:kinoteatr@cinema.chita",
-        mail: 'kinoteatr@cinema.chita',
         key: 4
     }
 ]
@@ -34,22 +33,24 @@ const asideInformation = [
 export function ContactsPage (){
     return(
         <section className="contactsSection">
-            <h2 className='contactsSection__title'>Сотрудники киноклуба «Москва» будут рады ответить на все интересующие вопросы, связанные с работой кинотеатра и ресторанов</h2>
-            <section>
-            {
-                asideInformation.map((asideInf) => {
-                return(
-                        <article className="contacts__article" key={asideInf.key}>
-                        <aside className="contacts__aside">
-                            <p className="contacts__title">{asideInf.title} &laquo;Cinema Chita&raquo;</p>
-                            <p className="contacts__number">{asideInf.number}</p>
-                            <a className="contacts__mail" href={asideInf.hrefMail}></a>
-                        </aside>
-                    </article>
-                )}
-                )
-            }
-            </section>
+            <div className="container">
+                <h2 className='contactsSection__title'>Сотрудники киноклуба «Москва» будут рады ответить на все интересующие вопросы, связанные с работой кинотеатра и ресторанов</h2>
+                <section className='sectionAside'>
+                {
+                    asideInformation.map((asideInf) => {
+                    return(
+                            <article className="contacts__article" key={asideInf.key}>
+                            <aside className="contacts__aside">
+                                <p className="contacts__title">{asideInf.title} &laquo;Cinema Chita&raquo;</p>
+                                <p className="contacts__number">{asideInf.number}</p>
+                                <a className="contacts__mail" href={asideInf.hrefMail}>{asideInf.mail}</a>
+                            </aside>
+                        </article>
+                    )}
+                    )
+                }
+                </section>
+            </div>
         </section>
     )
 }

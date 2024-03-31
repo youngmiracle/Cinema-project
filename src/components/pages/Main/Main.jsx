ed5dfbaa0dd89167134f04c7f69030a3f32c05ab
Artem <partem645@gmail.com>
2024-03-31 16:40:57 +0300

    delete server in  react project

--- src/components/pages/Main/Main.jsx
+++ src/components/pages/Main/Main.jsx
@@ -1,6 +1,27 @@
 import './main.css'
+import React, { useEffect, useState } from 'react';
 
 export function MainPage(){
+    
+    const [state, setState] = useState(null);
+
+  const callBackendAPI = async () => {
+    const response = await fetch('/');
+    const body = await response.json();
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body;
+  };
+  
+  // получение GET маршрута с сервера Express, который соответствует GET из server.js 
+  useEffect(() => {
+    callBackendAPI()
+    .then(res => setState(res.express))
+    .catch(err => console.log(err));
+  }, [])
+
     return(
         <section className='mainPage'>
             <h1 style={{
